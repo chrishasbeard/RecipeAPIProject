dcc9c683517281ea3df5552900c5de83f9aece84
chrishasbeard <chrisholmes16@gmail.com>
2019-11-26 13:42:22 -0500

    first save

---
+++ RecipeAPIProject/Controllers/RecipeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace RecipeAPIProject.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public readonly HttpClient _client;
+        
+        public RecipeController(IHttpClientFactory client)
+        {
+            _client = client.CreateClient();
+            _client.BaseAddress = new Uri("http://www.recipepuppy.com/api/");
+
+        }
+        
+        public async Task<IActionResult> DisplayRecipe()
+        {
+            _client.BaseAddress = new Uri("http://www.recipepuppy.com/api/");
+            var serializer = new DataContractJsonSerializer(typeof(Recipe));
+            var response = await _client.GetStreamAsync("");
+            var results = serializer.ReadObject(response) as Recipe;
+
+             return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
